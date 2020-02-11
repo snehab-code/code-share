8ba3e1b5b8df673d510837711b05f424e68da023
Sneha Bhat <nehabphn@gmail.com>
2020-02-11 23:01:59 +0530

    config for deploy

--- config/server.js
+++ config/server.js
@@ -4,11 +4,11 @@ const server = app.listen(port, () => {
     console.log('listening on ' + port)
     const router = require('./routes')
     app.use('/', router)
-    // const path = require('path') 
-    // const dirPath = __dirname.replace('\config', '')
-    // app.get("*",(req,res) => { 
-    // res.sendFile(path.join(dirPath + "/client/build/index.html")) 
-    // }) 
+    const path = require('path') 
+    const dirPath = __dirname.replace('\config', '')
+    app.get("*",(req,res) => { 
+    res.sendFile(path.join(dirPath + "/client/build/index.html")) 
+    }) 
 }) 
 
 module.exports = server
